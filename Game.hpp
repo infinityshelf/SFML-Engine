8777a3d326e72d9d22551882cf07cdab07c9615b
Estevan Hernandez <estevan@bluerocket.us>
2017-08-04 14:59:15 -0700

    Game class, static main function that main.cpp calls

---
+++ Game.hpp
@@ -0,0 +1,18 @@
+//
+// Created by Estevan Hernandez on 8/4/17.
+//
+
+#ifndef SFML_ENGINE_GAME_HPP
+#define SFML_ENGINE_GAME_HPP
+
+#include "World.hpp"
+#include <SFML/Window.hpp>
+
+
+class Game {
+public:
+    static int main(int argc, const char *argv[]);
+};
+
+
+#endif //SFML_ENGINE_GAME_HPP
