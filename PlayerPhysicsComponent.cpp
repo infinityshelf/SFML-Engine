6edbac02c437f0853c2644600508f607872618f7
Estevan Hernandez <estevan@bluerocket.us>
2017-08-04 01:23:24 -0700

    renaming bbox to boundingBox_, renaming other members as well as making some private. simplyfing and cleaning up the collision and movement logic. adding a method placeFree(), onGround(), and clampValues

--- PlayerPhysicsComponent.cpp
+++ PlayerPhysicsComponent.cpp
@@ -7,53 +7,69 @@
 const bool debug = false;
 
 PlayerPhysicsComponent::PlayerPhysicsComponent(Entity &entity): PhysicsComponent(entity) {
-    bbox.width = 35 * 2;
-    bbox.height = 81 * 2;
-    position.x = 620 / 2 - bbox.width / 2;
-    position.y = 480 / 2 - bbox.height / 2;
+    boundingBox_.width = 35 * 2;
+    boundingBox_.height = 81 * 2;
+    position_.x = 620 / 2 - boundingBox_.width / 2;
+    position_.y = 480 / 2 - boundingBox_.height / 2;
 
+    max_left = max_right = 15;
+    max_up = 50;
+    max_down = 15;
+
+}
+
+void PlayerPhysicsComponent::clampValues(sf::Vector2i *vector) {
+    vector->x = (vector->x > max_right) ? max_right : vector->x;
+    vector->x = (vector->x < -max_left) ? -max_left : vector->x;
+    vector->y = (vector->y > max_down) ? max_down : vector->y;
+    vector->y = (vector->y < -max_up) ? -max_up : vector->y;
 }
 
 void PlayerPhysicsComponent::update(double elapsed) {
     if (debug) std::cout << "elapsed: " << elapsed << std::endl;
 
+
+    boundingBox_.left = position_.x;
+    boundingBox_.top = position_.y;
+
+
     proposedVector_->y += 2; //gravity?
 
-    bbox.left = position.x;
-    bbox.top = position.y;
-    projected_bbox = bbox;
-
-    bool xCollision = false;
-    bool yCollision = false;
-    for (sf::IntRect *rect: world_.collidables) {
-        //check x
-        projected_bbox.top = bbox.top;
-        projected_bbox.left = bbox.left + proposedVector_->x;
-
-        if (projected_bbox.intersects(*rect)) {
-            xCollision = true;
-            if (proposedVector_->x > 0) {
-                position.x = rect->left - bbox.width;
-            } else if (proposedVector_->x < 0) {
-                position.x = rect->left + rect->width;
-            }
-        }
-        //check y
-        projected_bbox.left = bbox.left;
-        projected_bbox.top = bbox.top + proposedVector_->y;
-        if (projected_bbox.intersects(*rect)) {
-            yCollision = true;
-            if (proposedVector_->y > 0) {
-                position.y = rect->top - bbox.height;
-            } else if (proposedVector_->y < 0) {
-                position.y = rect->top + rect->height;
-            }
+    if (placeFree(position_.x, position_.y - 1, boundingBox_) != nullptr) {
+        if (proposedVector_->y < 0) {
+            proposedVector_->y = 0;
         }
     }
-    if (xCollision == false) {
-        position.x += proposedVector_->x;
+
+    clampValues(proposedVector_);
+
+    const sf::IntRect *result;
+
+    result = placeFree(position_.x + proposedVector_->x, position_.y, boundingBox_);
+
+    if (result == nullptr) {
+        position_.x += proposedVector_->x;
+    } else {
+        if (proposedVector_->x > 0) {
+            position_.x = result->left - boundingBox_.width;
+        } else if (proposedVector_->x < 0) {
+            position_.x = result->left + result->width;
+        }
     }
-    if (yCollision == false) {
-        position.y += proposedVector_->y;
+
+    result = placeFree(position_.x , position_.y + proposedVector_->y, boundingBox_);
+
+    if (result == nullptr) {
+        position_.y += proposedVector_->y;
+    } else {
+        if (proposedVector_->y > 0) {
+            position_.y = result->top - boundingBox_.height;
+        } else if (proposedVector_->y < 0) {
+            position_.y = result->top + result->height;
+        }
     }
+}
+
+bool PlayerPhysicsComponent::onGround() {
+    return (placeFree(position_.x, position_.y - 1, boundingBox_) == nullptr) ? true : false;
 }
